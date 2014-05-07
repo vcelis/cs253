a1ceb54d54e82171c0d681aecd7fd158d0ef7a67
vcelis <vincent.celis@gmail.com>
2014-05-07 18:04:53 +0200

    Finished lesson3 - homework1

--- lesson3/homework1/classes/homepage.py
+++ lesson3/homework1/classes/homepage.py
@@ -8,5 +8,9 @@ from google.appengine.ext import db
 
 class HomePage(basehandler.BaseHandler):
   def get(self, idx=''):
+    #test = post.Post.get_by_id(int(idx))
+    if idx and not post.Post.get_by_id(int(idx)):
+      self.error(404)
+      return
     posts = [post.Post.get_by_id(int(idx))] if idx else db.GqlQuery('SELECT * FROM Post ORDER BY time DESC LIMIT 10')
     self.render('home.html', posts=posts)
